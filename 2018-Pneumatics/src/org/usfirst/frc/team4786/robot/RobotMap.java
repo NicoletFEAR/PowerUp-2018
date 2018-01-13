87a7fdf427b5e532f56d688382245d7d291f57a0
Nhut Ly <jacknhutly@gmail.com>
2018-01-13 17:00:31 -0600

    First Commit to Pneumatic - pistons bound to bumpers

---
+++ 2018-Pneumatics/src/org/usfirst/frc/team4786/robot/RobotMap.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team4786.robot;
+
+/**
+ *
+ */
+public class RobotMap {
+	public final static int moduleNumber = 1;
+	public final static int forwardChannel = 1;
+	public final static int reverseChannel = 2;
+	
+
+	{
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+      //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
---
+++ 2018-Pneumatics/src/org/usfirst/frc/team4786/robot/xbox/Buttons.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team4786.robot.xbox;
+
+/**
+ *
+ */
+public class Buttons {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+	//xbox controller
+		public static final int A = 1;
+		public static final int B = 2;
+		public static final int X = 3;
+		public static final int Y = 4;
+		public static final int LeftBump = 5;
+		public static final int RightBump = 6;
+		public static final int Back = 7;
+		public static final int Start = 8;
+		public static final int LeftStick = 9;
+		public static final int RightStick = 10;
+}
+
