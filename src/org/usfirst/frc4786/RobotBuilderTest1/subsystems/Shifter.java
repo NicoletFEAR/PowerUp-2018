2e9ab64d5acbdf98b4427e8d3a1b8b0739b2f775
Fear4 <Fear4@15452FEAR>
2018-02-16 19:47:55 -0600

    updated pnuematics pistons and stuff.

--- src/org/usfirst/frc4786/RobotBuilderTest1/subsystems/Shifter.java
+++ src/org/usfirst/frc4786/RobotBuilderTest1/subsystems/Shifter.java
@@ -11,44 +11,38 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  *
  */
 public class Shifter extends Subsystem {
-	private final DoubleSolenoid leftShifter = new DoubleSolenoid(RobotMap.aForwardChannel, RobotMap.aReverseChannel);
-	// private final DoubleSolenoid rightShifter = new
-	// DoubleSolenoid(RobotMap.bForwardChannel, RobotMap.bReverseChannel);
+	private final DoubleSolenoid shifty = new DoubleSolenoid(RobotMap.module0, RobotMap.aForwardChannel,
+			RobotMap.aReverseChannel);
 	public static boolean nomatterwhat = false;
 
 	public void shiftup() {
-		leftShifter.set(DoubleSolenoid.Value.kForward);
-		// rightShifter.set(DoubleSolenoid.Value.kForward);
+		shifty.set(DoubleSolenoid.Value.kForward);
 	}
 
 	public void shiftdown() {
-		leftShifter.set(DoubleSolenoid.Value.kReverse);
-		// rightShifter.set(DoubleSolenoid.Value.kReverse);
+		shifty.set(DoubleSolenoid.Value.kReverse);
 	}
 
 	// shift the gearbox to the opposite state
 	public void shift() {
-		if (leftShifter.get() == DoubleSolenoid.Value.kForward) {
+		if (shifty.get() == DoubleSolenoid.Value.kForward) {
 			shiftdown();
 		} else {
 			shiftup();
 		}
-
 	}
 
 	public void stop() {
-		leftShifter.set(DoubleSolenoid.Value.kOff);
+		shifty.set(DoubleSolenoid.Value.kOff);
 	}
 
 	public boolean getNoMatterWhat() {
 		return nomatterwhat;
-
 	}
 
 	public void setNoMatterWhat() {
 		nomatterwhat = !nomatterwhat;
 		SmartDashboard.putBoolean("Start button hit", Robot.shifter.getNoMatterWhat());
-
 	}
 
 	// Put methods for controlling this subsystem
@@ -56,6 +50,5 @@ public class Shifter extends Subsystem {
 	// make sure the pistons are closed at first
 	public void initDefaultCommand() {
 		shiftdown();
-
 	}
 }
