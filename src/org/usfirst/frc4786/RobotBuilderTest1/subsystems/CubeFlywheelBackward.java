cd8b9dbd8fc1968e0d00795fdd8289797fbc7d62
Leighton <leightonkennedy7@gmail.com>
2018-02-16 19:17:41 -0600

    merging in cube branch

---
+++ src/org/usfirst/frc4786/RobotBuilderTest1/subsystems/CubeFlywheelBackward.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc4786.RobotBuilderTest1.subsystems;
+
+import org.usfirst.frc4786.RobotBuilderTest1.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class CubeFlywheelBackward extends Command {
+
+    public CubeFlywheelBackward() {
+        // Use requires() here to declare subsystem dependencies
+    	requires(Robot.cubeMech);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.cubeMech.cubeFlywheelBack();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
