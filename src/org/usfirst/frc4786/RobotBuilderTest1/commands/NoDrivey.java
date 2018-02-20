98a7377f2f9c366dae147fbc840e639ae226a6fd
CodeforEvolution <themysterymail555@gmail.com>
2018-02-19 18:08:05 -0600

    Another Fix to NoDrivey

--- src/org/usfirst/frc4786/RobotBuilderTest1/commands/NoDrivey.java
+++ src/org/usfirst/frc4786/RobotBuilderTest1/commands/NoDrivey.java
@@ -4,9 +4,6 @@ import org.usfirst.frc4786.RobotBuilderTest1.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
-/**
- *
- */
 public class NoDrivey extends Command {
 
     public NoDrivey() {
@@ -16,20 +13,21 @@ public class NoDrivey extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.driveTrain.driveToPositionInit(0);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.driveTrain.stop();
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return Robot.driveTrain.driveToPositionIsFinished();
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.driveTrain.driveToPositionEnd();
     }
 
     // Called when another command which requires one or more of the same
