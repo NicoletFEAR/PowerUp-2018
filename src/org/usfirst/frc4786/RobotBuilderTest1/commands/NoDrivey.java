9f2bc246ed374a7e6f51dae99024acf8cff70ee8
CodeforEvolution <themysterymail555@gmail.com>
2018-02-19 18:08:46 -0600

    Another Fix to No Drivey

--- src/org/usfirst/frc4786/RobotBuilderTest1/commands/NoDrivey.java
+++ src/org/usfirst/frc4786/RobotBuilderTest1/commands/NoDrivey.java
@@ -30,6 +30,7 @@ public class NoDrivey extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.driveTrain.driveToPositionEnd();
     }
 
     // Called when another command which requires one or more of the same
